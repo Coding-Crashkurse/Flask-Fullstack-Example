180429263a2413b79834976b46f8d78ebbad712e
markus <datamastery87@gmail.com>
2022-12-05 20:05:36 +0100

    add templates

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length
 from flask_wtf import FlaskForm
 
+db = SQLAlchemy()
+
 app = Flask(__name__)
-app.secret_key = "secret"
+app.config["SECRET_KEY"] = "secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 bootstrap = Bootstrap5(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 150)])
+    repeatedpassword = PasswordField('Wiederhole Passwort', validators=[DataRequired(), Length(8, 150)])
+    submit = SubmitField()
 
 
 class LoginForm(FlaskForm):
@@ -17,13 +38,31 @@ class LoginForm(FlaskForm):
 
 
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash('Form validated!')
+        return redirect(url_for("index"))
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Form validated!')
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form)
+        return redirect(url_for("index"))
+    return render_template("login.html", form=form)
+
+
+@app.route("/dashboard", methods=["POST", "GET"])
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == '__main__':
